4358bf2026c3cb9a5fdeb0a62625eae9fc1d497c
Nika Ilieva <nika.ilieva@duke.edu>
2026-07-09 17:58:05 -0400

    Code to check sensor function

--- src/main.c
+++ src/main.c
@@ -1,22 +1,49 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/sensor.h>
-#include <zephyr/drivers/i2c.h>
+#include <zephyr/logging/log.h>
 
-#define IMU_NODE DT_NODELABEL(lsm6dsl)
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-static const struct device *imu;
+#define IMU_NODE DT_NODELABEL(mpu6050)
 
 int main(void)
 {
-    imu = DEVICE_DT_GET(IMU_NODE);
-// check that device is ready (dev academy taught me well)
-    if (!device_is_ready(imu)) {
-        LOG_ERR("LSM6DSL not ready!");
-        return -ENODEV;
-    }
-    LOG_INF("LSM6DSL found: %s", imu->name);
+	const struct device *imu = DEVICE_DT_GET(IMU_NODE);
 
-    configure_imu(imu);
+	if (!device_is_ready(imu)) {
+		printk("MPU-6050 not ready -- check wiring, overlay, and prj.conf\n");
+		return -ENODEV;
+	}
 
+	printk("MPU-6050 ready: %s\n", imu->name);
+
+	while (1) {
+		struct sensor_value ax, ay, az, gx, gy, gz;
+
+		int rc = sensor_sample_fetch(imu);
+
+		if (rc != 0) {
+			printk("sensor_sample_fetch failed: %d\n", rc);
+			k_msleep(500);
+			continue;
+		}
+
+		sensor_channel_get(imu, SENSOR_CHAN_ACCEL_X, &ax);
+		sensor_channel_get(imu, SENSOR_CHAN_ACCEL_Y, &ay);
+		sensor_channel_get(imu, SENSOR_CHAN_ACCEL_Z, &az);
+		sensor_channel_get(imu, SENSOR_CHAN_GYRO_X, &gx);
+		sensor_channel_get(imu, SENSOR_CHAN_GYRO_Y, &gy);
+		sensor_channel_get(imu, SENSOR_CHAN_GYRO_Z, &gz);
+
+		printk("accel (m/s^2): x=%d.%06d y=%d.%06d z=%d.%06d   "
+		       "gyro (rad/s): x=%d.%06d y=%d.%06d z=%d.%06d\n",
+		       ax.val1, ax.val2, ay.val1, ay.val2, az.val1, az.val2,
+		       gx.val1, gx.val2, gy.val1, gy.val2, gz.val1, gz.val2);
+
+		k_msleep(500);
+	}
+
+	return 0;
 }
